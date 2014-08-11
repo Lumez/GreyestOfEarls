ab0555b45fab452072b94efa32216213247fbfe5
Ben <lumezious@googlemail.com>
2014-08-11 10:58:57 +0100

    Remove Info

--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  consumer_key: 'aJrODnPxIqbvyDUBIuTbQAwZd',
-  consumer_secret: 'pvgYEsa11AQxoPIYSWWyam89Si4z15yC5q7MvuCSqjAaM87U6r',
-  access_token: '2716624266-qbWoiH6isA7YnuMBsCkSxA5ZFhQYsLQJQ80MBwR',
-  access_token_secret: '3f1LaQVkaqAVh9sBy5APTxHTS7dwJJkcIp9Ukud5OsB9d'
-};
